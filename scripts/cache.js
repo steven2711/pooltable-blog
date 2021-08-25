251aed0cbba6550f3d098b7f95801f8b96ba9028
Steven Watson <swatson.emu@gmail.com>
2021-08-25 01:23:26 -0500

    Deleted old posts and added first blog post. Woot!

--- scripts/cache.js
+++ scripts/cache.js
@@ -1,39 +1,36 @@
- const fs = require('fs')
- const path = require('path')
- const matter = require('gray-matter')
-
-
+const fs = require("fs");
+const path = require("path");
+const matter = require("gray-matter");
 
 function postData() {
-    const files = fs.readdirSync(path.join('posts'))
+  const files = fs.readdirSync(path.join("posts"));
 
-    const posts = files.map((filename) => {
-        const slug = filename.replace('.md', '')
-    
-        const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8')
-    
-        const {data:frontmatter} = matter(markdownWithMeta)
-    
-        return {
-          slug,
-          frontmatter
-        }
-    
-      })
+  const posts = files.map((filename) => {
+    const slug = filename.replace(".md", "");
 
+    const markdownWithMeta = fs.readFileSync(
+      path.join("posts", filename),
+      "utf-8"
+    );
 
-      return `export const posts = ${JSON.stringify(posts)}`
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return `export const posts = ${JSON.stringify(posts)}`;
 }
-
 
 try {
-     fs.readdirSync('cache')
+  fs.readdirSync("cache");
 } catch (error) {
-    fs.mkdirSync('cache')
+  fs.mkdirSync("cache");
 }
 
-
-fs.writeFile('cache/data.js', postData(), function(err){
-    if(err) return console.log(err)
-    console.log('Posts cached...')
-})
+fs.writeFile("cache/data.js", postData(), function (err) {
+  if (err) return console.log(err);
+  console.log("Posts cached...");
+});
