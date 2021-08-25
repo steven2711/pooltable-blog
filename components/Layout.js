03171ad81db4f7cb2cb99ab310a00bd4ff183754
Steven Watson <swatson.emu@gmail.com>
2021-08-25 02:10:19 -0500

    Added google analytics to layout.

--- components/Layout.js
+++ components/Layout.js
@@ -46,6 +46,27 @@ export default function Layout({
           content="-JvaYpKFpaQTbRpkLKV5DpaWlKRjs2Zwve8w-w3EZ44"
         />
 
+        {/* Google Analytics*/}
+
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+
         {/* Rich Search Result Scripts */}
       </Head>
       <Header />
